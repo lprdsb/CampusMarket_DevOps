54ec297911c07edb74a037694dc621bf864404e3
lpr <948392651@qq.com>
2025-05-22 13:15:54 +0800

    upd star

---
+++ CampusMarket-sys/src/main/java/cn/pojo/dto/query/extend/starQueryDto.java
@@ -0,0 +1,21 @@
+package cn.pojo.dto.query.extend;
+
+import cn.pojo.dto.query.base.QueryDto;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 互动行为查询条件类
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class starQueryDto extends QueryDto {
+    /**
+     * 用户ID
+     */
+    private Integer user1Id;
+    /**
+     * 商品ID
+     */
+    private Integer user2Id;
+}
